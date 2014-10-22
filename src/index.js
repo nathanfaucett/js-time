ecd41fb564bf91985f67556f95cd2bff92b0d3aa
nathanfaucett <nathanfaucett@gmail.com>
2014-10-22 11:40:50 -0500

    fixed node now

--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ Date.now || (Date.now = function now() {
 
 if (!process.browser) {
     HR_TIME = process.hrtime();
+
     now = function now() {
         var hrtime = process.hrtime(HR_TIME),
-            s = hrtime[0] * 1e-3,
+            ms = hrtime[0] * 1e3,
             ns = hrtime[1] * 1e-6;
 
-        return s + ns;
+        return ms + ns;
     };
 } else {
     performance = global.performance || {};
@@ -38,7 +39,7 @@ START_MS = Date.now();
 
 time.now = now;
 
-time.stamp = function() {
+time.stamp = function stamp() {
 
-    return START_MS + time.now();
+    return START_MS + now();
 };
